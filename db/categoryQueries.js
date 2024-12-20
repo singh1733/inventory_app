0b7767c6a8a21707b75eab805d88c351db56174c
singh1773 <singh.1733@osu.edu>
2024-12-20 15:10:31 -0500

    Made queries for both categories and items following CRUD

---
+++ db/categoryQueries.js
@@ -0,0 +1,33 @@
+const pool = require("./pool");
+
+async function getAllCategories() {
+  const { rows } = await pool.query("SELECT * FROM categories");
+  return rows;
+}
+
+async function insertCategory(name) {
+  await pool.query("INSERT INTO categories (name,) VALUES ($1)", [name]);
+}
+
+async function getCategoryByID(id) {
+  const { rows } = await pool.query("SELECT * FROM categories WHERE id = $1", [
+    id,
+  ]);
+  return rows;
+}
+
+async function getCategoryItems(category_id) {
+  const { rows } = await pool.query(
+    "SELECT * FROM categories JOIN items ON categories.id=items.category_id WHERE categories.id=$1",
+    [category_id]
+  );
+  return rows;
+}
+
+async function updateCategory(id, name) {
+  await pool.query("UPDATE categories SET name = $1 WHERE id=$2", [name, id]);
+}
+
+async function deleteItem(id) {
+  await pool.query("DELETE FROM categories WHERE id = $1", [id]);
+}
